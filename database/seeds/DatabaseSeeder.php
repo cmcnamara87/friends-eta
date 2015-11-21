816ca6bfa7740a534f4e42d86452ea860c97ce68
Craig McNamara <cmcnamara87@gmail.com>
2015-11-21 12:33:35 +1000

    feat(seeder): add database seeder with dummy data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,13 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
-
+        $craig = \App\User::create(['name' => 'Craig', 'email' => 'craig@asd.com']);
+        $craigLocation = \App\Location::create(['user_id' => 1, 'lat' => -27.47101, 'long' => 153.02345]);
+        $kelvin = \App\User::create(['name' => 'Kelvin', 'email' => 'kel@asd.com']);
+        $kelvinLocation = \App\Location::create(['user_id' => 2, 'lat' => -27.37593, 'long' => 153.12095]);
+        $jamal = \App\User::create(['name' => 'Jamal', 'email' => 'jamal@asd.com']);
+        $jamalLocation = \App\Location::create(['user_id' => 3, 'lat' => -26.37593, 'long' => 152.12095]);
+        
         Model::reguard();
     }
 }

325e87e12f5b7fa76a4fcdb947334bcdf65ec122
Craig McNamara <cmcnamara87@gmail.com>
2016-01-12 16:59:28 +1000

    refactor(users): remove password

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('fb_id', 50);
             $table->string('email')->unique();
-            $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
         });

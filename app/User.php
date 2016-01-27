bcc6f33dcc590a7899040054a7e0ad635388e775
Craig McNamara <cmcnamara87@gmail.com>
2016-01-27 21:13:30 +1000

    add picture to fillable and hide email and fb id from json respone

--- app/User.php
+++ app/User.php
@@ -28,12 +28,12 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password', 'fb_id'];
+    protected $fillable = ['name', 'email', 'password', 'fb_id', 'picture'];
 
     /**
      * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['password', 'remember_token', 'email', 'fb_id'];
 }
